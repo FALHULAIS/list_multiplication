8401da56a20557c980d119b00629a02889da4d46
FAHAD ALI ALHULAIS <36438317+FALHULAIS@users.noreply.github.com>
2019-06-10 22:18:05 +0300

    Add files via upload

---
+++ List_multiply.py
@@ -0,0 +1,9 @@
+def multiply_list(list):
+       i = 1
+       total =1
+       for i in list:
+              i = i * 1
+              total = total * i
+       print(total)
+
+multiply_list([1,13,15,10])
